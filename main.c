618827541855d5a2083943846bd02aca1f51a118
WolfyCodek <45407550+WolfyCodeK@users.noreply.github.com>
2023-03-09 02:22:14 +0000

    changed window title

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[]) {
     SDL_Renderer* renderer;
 
     SDL_CreateWindowAndRenderer(winWidth, winHeight, 0, &win, &renderer);
+    SDL_SetWindowTitle(win, "SDL PROJECT");
 
     while (1) {
         SDL_Event winEvent;
