05294e17d3041ab19514a29c708f3abff8dd6bc9
WolfyCodek <45407550+WolfyCodeK@users.noreply.github.com>
2023-03-10 06:44:55 +0000

    created vector functions and struct

--- main.c
+++ main.c
@@ -1,11 +1,63 @@
 #include<stdio.h>
+#include<math.h>
 #include<SDL2/SDL.h>
 
+#define winWidth 600
+#define winHeight 600
+
+struct vector {
+  float x;
+  float y;
+  float z;
+};
+
+float magnitude(double x, double y, double z) {
+    return sqrt(x * x + y * y + z * z);
+}
+
+void normalise(double x, double y, double z) {
+    double mag = magnitude(x, y, z);
+
+    if (mag != 0) {
+      x /= mag;
+      y /= mag;
+      z /= mag;
+    }
+}
+
+struct vector cross(struct vector v1, struct vector v2) {
+    struct vector v = {
+      (v1.y * v2.z) - (v1.z * v2.y),
+      (v1.z * v2.x) - (v1.x * v2.z),
+      (v1.x * v2.y) - (v1.y * v2.x)
+    };
+
+    return v;
+}
+
+double dot(struct vector v1, struct vector v2) {
+  return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z;
+}
+
+struct vector sub(struct vector v1, struct vector v2) {
+  struct vector v = {v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
+
+  return v;
+}
+
+struct vector add(struct vector v1, struct vector v2) {
+  struct vector v = {v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
+
+  return v;
+}
+
+struct vector mul(struct vector v1, float n) {
+    struct vector v = {n * v1.x, n * v1.y, n * v1.z};
+
+    return v;
+  }
+
 int main(int argc, char *argv[]) {
-
-    const int winWidth = 600;
-    const int winHeight = 600;
-
     SDL_Init(SDL_INIT_VIDEO);   
 
     SDL_Window* win;
@@ -29,10 +81,24 @@ int main(int argc, char *argv[]) {
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
+        /*  
         SDL_RenderDrawLine(renderer, winWidth / 2, winHeight / 2, 0, 0);
         SDL_RenderDrawLine(renderer, winWidth / 2, winHeight / 2, winWidth, 0);
         SDL_RenderDrawLine(renderer, winWidth / 2, winHeight / 2, 0, winHeight);
         SDL_RenderDrawLine(renderer, winWidth / 2, winHeight / 2, winWidth, winHeight);
+        */
+
+        
+
+        struct vector centerSphere = {0, 0, 0};
+
+        
+
+        for (int j = 0; j < winHeight; j++) {
+            for (int i = 0; i < winWidth; i++) {
+
+            }
+        }
 
         SDL_RenderPresent(renderer);
     }
